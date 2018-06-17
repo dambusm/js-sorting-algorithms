5b0f668072b8c5f9aa860c30c70322c5e03569ab
Will Schaller <willschaller@gmail.com>
2018-06-17 12:34:02 +0200

    Improve bubble sort, only repeat until no swaps

--- main.js
+++ main.js
@@ -28,14 +28,18 @@ function selectionSort(array) {
 // Bubble sort
 
 function bubbleSort(array) {
-  for (var i = array.length - 1; i >= 0; i--) {
-    for (var j = 1; j <= i; j++) {
-      if (array[j - 1] > array[j]) {
-        var temp = array[j - 1];
-        array[j - 1] = array[j];
-        array[j] = temp;
+  do {
+    var swapped = false;
+    for (var i = array.length - 1; i >= 0; i--) {
+      for (var j = 1; j <= i; j++) {
+        if (array[j - 1] > array[j]) {
+          var temp = array[j - 1];
+          array[j - 1] = array[j];
+          array[j] = temp;
+          swapped = true;
+        }
       }
     }
-  }
+  } while (swapped);
   return array;
 }
